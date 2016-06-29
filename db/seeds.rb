f8395c5bbd7ae68952af1b0825b72dc47c30cafc
alexhimmel <alex.ac@qq.com>
2016-06-29 15:08:29 +0800

    finish user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "Alex Himmel",
+             email: "alex.ac@qq.com",
+             password:              "7787782",
+             password_confirmation: "7787782",
+             admin: true)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "alex-#{n+1}@qq.com"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
