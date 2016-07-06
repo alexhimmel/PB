331a13888d0763856e48ecaba3d6079612d41b8f
alexhimmel <alex.ac@qq.com>
2016-07-06 10:34:33 +0800

    add S3 region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,9 @@ if Rails.env.production?
       # Amazon S3 的配置
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      # 区域选择
+      # :region                => ENV['S3_REGION']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
